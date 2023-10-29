fe77164a4e12b976334101b8c828ceee2bb7a447
Lasya Kambhampati <84462162+lasya02@users.noreply.github.com>
2023-10-29 11:15:44 -0400

    finished all UI

--- Sherpa/Sherpa/View/Result.swift
+++ Sherpa/Sherpa/View/Result.swift
@@ -11,85 +11,115 @@ struct Result: View {
     @StateObject var resultVM: ResultViewModel = .init()
     @Namespace var animation
     
+    // @State var gptFunction: gptFunction = gptFunction()
+    
     var vacationDays: [Date] = []
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            LazyVStack(spacing: 15, pinnedViews: [.sectionHeaders]) {
-               
-                Section {
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 10) {
-                            ForEach(self.resultVM.currentWeek, id: \.self) { day in
-                                
-                                Button {
-                                    // change what part of the itinerary is shown
-                                } label: {
-                                    VStack(spacing: 10) {
-                                        Text(self.resultVM.extractDate(date: day, format: "dd"))
-                                            .font(.system(size: 14))
-                                            .fontWeight(.semibold)
-                                        
-                                        Text(self.resultVM.extractDate(date: day, format: "EEE"))
-                                            .font(.system(size: 14))
-                                        
-                                        Circle()
-                                            .fill(.white)
-                                            .frame(width: 8, height: 8)
-                                            .opacity(self.resultVM.isToday(date: day) ? 1 : 0)
-                                    }
-                                    .foregroundStyle(self.resultVM.isToday(date: day) ? .primary : .secondary)
-                                    .foregroundColor(self.resultVM.isToday(date: day) ? .white : .black)
-                                    .frame(width: 45, height: 90)
-                                    .background(
-                                        ZStack {
-                                            if self.resultVM.isToday(date: day) {
-                                                Capsule()
-                                                    .fill(.black)
-                                                    .matchedGeometryEffect(id: "CURRENTDAY", in: self.animation)
+        ZStack {
+            Color.darkBlue.opacity(0.8)
+                .ignoresSafeArea()
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVStack(spacing: 15, pinnedViews: [.sectionHeaders]) {
+                    Section {
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 10) {
+                                ForEach(self.resultVM.currentWeek, id: \.self) { day in
+                                    
+                                    Button {
+                                        // change what part of the itinerary is shown
+                                    } label: {
+                                        VStack(spacing: 10) {
+                                            Text(self.resultVM.extractDate(date: day, format: "dd"))
+                                                .font(.system(size: 14))
+                                                .fontWeight(.semibold)
+                                            
+                                            Text(self.resultVM.extractDate(date: day, format: "EEE"))
+                                                .font(.system(size: 14))
+                                            
+                                            Circle()
+                                                .fill(.white)
+                                                .frame(width: 8, height: 8)
+                                                .opacity(self.resultVM.isToday(date: day) ? 1 : 0)
+                                        }
+                                        .foregroundStyle(self.resultVM.isToday(date: day) ? .primary : .secondary)
+                                        .foregroundColor(self.resultVM.isToday(date: day) ? .white : .black)
+                                        .frame(width: 45, height: 90)
+                                        .background(
+                                            ZStack {
+                                                if self.resultVM.isToday(date: day) {
+                                                    Capsule()
+                                                        .fill(.black)
+                                                        .matchedGeometryEffect(id: "CURRENTDAY", in: self.animation)
+                                                }
+                                            })
+                                        .contentShape(Capsule())
+                                        .onTapGesture {
+                                            withAnimation {
+                                                self.resultVM.currentDay = day
                                             }
-                                        })
-                                    .contentShape(Capsule())
-                                    .onTapGesture {
-                                        withAnimation {
-                                            self.resultVM.currentDay = day
                                         }
                                     }
                                 }
                             }
+                            .padding(.horizontal)
                         }
-                        .padding(.horizontal)
+                        Spacer()
+                        self.TaskView()
+                            .padding(.horizontal, 6)
+                    } header: {
+                        self.HeaderView()
                     }
-                    TaskView()
-                } header: {
-                    self.HeaderView()
                 }
             }
         }
     }
     
-    func TaskView() -> some View{
-        
-        LazyVStack (spacing: 18){
-            
+    func TaskView() -> some View {
+        LazyVStack(spacing: 80) {
+            ForEach(0 ..< 3) { timeIndex in
+                Section {
+                    HStack (spacing: 30) {
+                        Circle()
+                            .fill(.white)
+                            .frame(width: 15, height: 15)
+                            .background(
+                                Circle()
+                                    .stroke(.white, lineWidth: 2)
+                                    .padding(-5))
+                        Text("\(self.resultVM.getTasks(timeOfDay: timeIndex))")
+                            .multilineTextAlignment(.leading)
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.horizontal, 40)
+                    .hLeading()
+                    
+                }
+            }
+        }
+        .onChange(of: self.resultVM.currentDay) { _ in
+            self.resultVM.changeDate()
         }
     }
-    
+
     func HeaderView() -> some View {
         HStack(spacing: 20) {
             VStack(alignment: .leading, spacing: 10) {
                 Text(Date().formatted(date: .abbreviated, time: .omitted))
-                    .foregroundStyle(.gray)
+                    .foregroundStyle(.white)
                 Text("Today")
                     .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
             }
             .hLeading()
         }
         .padding()
-        .background(Color.white)
+        .background(Color.clear)
     }
 }
-
+    
 #Preview {
     Result()
 }
--- Sherpa/Sherpa/View/ResultViewModel.swift
+++ Sherpa/Sherpa/View/ResultViewModel.swift
@@ -7,29 +7,85 @@
 
 import SwiftUI
 
-class ResultViewModel: ObservableObject{
+class ResultViewModel: ObservableObject {
     @Published var currentWeek: [Date] = []
     
-    @Published var currentDay: Date = Date()
+    @Published var currentDay: Date = .init()
     
     @Published var filteredActivities: [String]?
     
-    init(){
+    @Published var day = 0
+    
+    @Published var storedActivities = [
+        [
+            ["Visit the North Carolina Research Campus"],
+            ["Explore the Kannapolis History Room at the Kannapolis Branch Library"],
+            ["Watch a movie at the Gem Theatre"]
+        ],
+        [
+            ["Go for a hike at Frank Liske Park"],
+            ["Visit Kannapolis City Hall and learn about the city's government"],
+            ["Dine at a local restaurant in downtown Kannapolis"]
+        ],
+        [
+            ["Take a stroll through Village Park and enjoy the scenery"],
+            ["Visit the Kannapolis Fire Department Museum"],
+            ["Attend a live music event at Veterans Park"]
+        ],
+        [
+            ["Visit the Dale Earnhardt Plaza and learn about the racing legend"],
+            ["Explore the North Carolina Music Hall of Fame"],
+            ["Enjoy a performance at the Kannapolis Performing Arts Center"]
+        ],
+        [
+            ["Visit the Old Courthouse Theatre and catch a play"],
+            ["Explore the Kannapolis Train Station and learn about its history"],
+            ["Have a picnic at the Fred M. Evans Amphitheater"]
+        ],
+        [
+            ["Go shopping at the Northlite Shopping Center"],
+            ["Visit the Kannapolis Recreation Park and enjoy outdoor activities"],
+            ["Attend a sports game at the A.L. Brown Stadium"]
+        ],
+        [
+            ["Visit the Curb Museum for Music and Motorsports"],
+            ["Explore the North Carolina Science Festival at the North Carolina Research Campus"],
+            ["Dine at a waterfront restaurant at Lake Fisher"]
+        ],
+        [
+            ["Go for a bike ride along the Kannapolis Greenway"],
+            ["Visit the Kannapolis Library for a book reading or workshop"],
+            ["Watch a play at the Kannapolis Little Theatre"]
+        ],
+        [
+            ["Take a yoga class at one of the local yoga studios"],
+            ["Visit the Canapples Flea Market for unique finds"],
+            ["Attend a cultural festival at the North Carolina Research Campus"]
+        ],
+        [
+            ["Explore the art galleries in downtown Kannapolis"],
+            ["Visit the Kannapolis Train Depot and learn about its history"],
+            ["Have dinner at a rooftop restaurant with a view of the city"]
+        ]
+    ]
+
+
+    init() {
         fetchCurrentWeek()
-        //filterTodayTasks()
+        // filterTodayTasks()
     }
     
 //    func filterTodayTasks(){
 //        DispatchQueue(qos:  .userInteractive).async {
 //            let calendar  = Calendar.current
-//            
 //
-//            
+//
+//
 //        }
-//        
+//
 //    }
     
-    func fetchCurrentWeek(){
+    func fetchCurrentWeek() {
         let today = Date()
         let calendar = Calendar.current
         
@@ -38,26 +94,38 @@ class ResultViewModel: ObservableObject{
         guard let firstWeekDay = week?.start else {
             return
         }
-        (1...7).forEach { day in
-            if let weekday = calendar.date(byAdding: .day, value: day, to: firstWeekDay){
+        (1 ... 7).forEach { day in
+            if let weekday = calendar.date(byAdding: .day, value: day, to: firstWeekDay) {
                 currentWeek.append(weekday)
             }
         }
     }
     
-    func extractDate(date: Date, format: String) -> String{
+    func extractDate(date: Date, format: String) -> String {
         let formatter = DateFormatter()
         
         formatter.dateFormat = format
         
         return formatter.string(from: date)
-        
     }
     
-    func isToday(date: Date)->Bool {
+    func isToday(date: Date) -> Bool {
         let calendar = Calendar.current
         
         return calendar.isDate(currentDay, inSameDayAs: date)
     }
     
+    func changeDate(){
+        if day == 9{
+            day = 0
+        }
+        else{
+            day = day + 1
+        }
+    }
+    
+    func getTasks(timeOfDay: Int)->String{
+        return storedActivities[day][timeOfDay][0]
+        
+    }
 }
